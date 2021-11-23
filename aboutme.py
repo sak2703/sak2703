ad6c4e8b0fdce1f36aa1091e0adb93cc3c358881
sak2703 <89259799+sak2703@users.noreply.github.com>
2021-11-23 11:40:35 +0530

    Create aboutme.py

---
+++ aboutme.py
@@ -0,0 +1,7 @@
+Hey, This is saketh ram 😁.
+
+A rookie in coding with a generic inspiration to change the world 😉
+
+Pursuing my undergrad from VITC, india in electronics and computer engineering
+
+
